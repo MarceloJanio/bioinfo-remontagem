74f11aa10534c78865b17330d9ebeb3855b9dd3e
MarceloJanio <marcelojanio@ufpi.edu.br>
2023-03-03 21:55:14 -0300

    atualizacoes

--- remontagem.py
+++ remontagem.py
@@ -1,7 +1,3 @@
-import time
-
-fita = ""
-
 def quebraPreSuf(listaFita, k):
     dictGraph = {}
     for i in listaFita:
@@ -25,40 +21,38 @@ def quebraPreSuf(listaFita, k):
 def findInitialnFinal(dictGraph):
     initial = ""
     final = ""
-
     for key, value in dictGraph.items():
         if(value[1][0] > value[1][1]):
             initial = key
         if(value[1][0] < value[1][1]):
             final = key
-    
     if(initial != "" and final != ""):
         print("{} {}".format(initial, final))
         return initial, final
 
 ## abre arquivo
-with open("exemplo2.txt", 'r') as file:
-    fita += file.read()
+
+def abreArquivo(nome, modo):
+    fita = ""
+    with open(nome, modo) as file:
+        fita += file.read()
 
 
-fita = fita.replace(' ', '')
+    fita = fita.replace(' ', '')
 
-if(fita[-1] == ','):
-    fita = fita[:-1]
+    if(fita[-1] == ','):
+        fita = fita[:-1]
 
-fita = fita.split(',')
-print(fita)
-k = len(fita[0])
-###########################
+    fita = fita.split(',')
+    k = len(fita[0])
+    return fita, k
 
 
-LinkedList = quebraPreSuf(fita,k)
-initial, final = findInitialnFinal(LinkedList)
+
 
 def montaEuleriano(dictGrafo, initial, k):
     primeiraParte = ""
     segundaParte = ""
-    sequencias = []
 
     fita = initial
     ponteiro = initial
@@ -90,35 +84,12 @@ def montaEuleriano(dictGrafo, initial, k):
 
     print(fita)
     print(dictGrafo)
-    print(sequencias)
 
-#GA GC CCCGC ACCACGAGGTCACGAG
-#GAGCCCGCCACCACGAGGTCACGAG
 
-# ['CC', 'CG']
-# {'CC': [['CC', ], [4, 4]], 'CG': [['GC'], [3, 3]], , [2, 2]]}
+
+fita, k  = abreArquivo('exemplo.txt', 'r')
+
+LinkedList = quebraPreSuf(fita,k)
+initial, final = findInitialnFinal(LinkedList)
 
 montaEuleriano(LinkedList, initial, k)
-
-# grafoG = grafo()
-
-# for i in LinkedList:
-#     grafoG.addAresta(i)
-#     grafoG.addNos(i.getNoInicial(), "pre")
-#     grafoG.addNos(i.getNoFinal(), "suf")
-    
-# for key, value in grafoG.getNos().items():
-#     if value[0] > value[1]:
-#         grafoG.setInitialNode(key)
-#     if value[1] > value[0]:
-#         grafoG.setFinalNode(key)
-
-#     if(grafoG.getFinalNode() != "" and grafoG.getInitialNode() != ""):
-#         break
-
-# print(grafoG.getNos())
-# print(grafoG.getInitialNode())
-# print(grafoG.getFinalNode())
-
-# def mountGraph(dnaFita, k):
-#     pass
